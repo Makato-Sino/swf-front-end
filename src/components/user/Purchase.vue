b3e15731dfcfaa194d19cadeb6db5a4874ae243d
hantch <850450571@qq.com>
2022-01-20 17:13:25 +0800

    修改purchase，现在可以正常购买流程

--- src/components/user/Purchase.vue
+++ src/components/user/Purchase.vue
@@ -5,29 +5,33 @@
 
     <!-- 购买服务表单 -->
     <el-dialog title="购买服务" :visible.sync="editDialogVisible" width="50%" @close="editDialogClosed">
-      <el-form label-width="70px">
+      <el-form label-width="70px" ref="importFormRef">
         <el-form-item label="流程名称" prop="processName">
             <el-input v-model="processName"></el-input>
         </el-form-item>
+      
+        <el-form-item label="上传文件" prop="uploadFile">
+          <el-upload
+            class="upload-demo"
+            ref="upload"
+            action="#"
+            :http-request="submitUpload"
+            :before-upload="beforeUpload"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :name="fileName"
+            :limit="1"
+            :file-list="fileList"
+            :auto-upload="false">
+            <el-button slot="trigger" size="small" type="primary">部署流程</el-button>
+            <!-- <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button> -->
+            <div slot="tip" class="el-upload__tip">只能上传bpmn/zip文件</div>
+          </el-upload>
+        </el-form-item>
       </el-form>
-      <el-upload
-          class="upload-demo"
-          ref="upload"
-          action=""
-          :http-request = submitUpload
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :name="fileName"
-          :limit="1"
-          :file-list="fileList"
-          :auto-upload="false">
-          <el-button slot="trigger" size="small" type="primary">部署流程</el-button>
-          <!-- <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button> -->
-          <div slot="tip" class="el-upload__tip">只能上传bpmn/zip文件</div>
-      </el-upload>
       <span slot="footer" class="dialog-footer">
           <el-button @click="editDialogVisible = false">取 消</el-button>
-          <!-- <el-button type="primary" @click="addUser">确 认</el-button> -->
+          <el-button type="primary" @click="submitImportForm">确 认</el-button>
       </span>
     </el-dialog>
 
@@ -46,21 +50,31 @@ export default {
     };
   },
   methods: {
-    async submitUpload(param) { 
-      const filelist = new FormData()
-      filelist.append('processFile',param.file)
-      console.log(param.file)
-      const {data:res} = await this.$http.post("processDefinition/uploadStreamAndDeployment?processName=" + this.processName + "&username=" + this.username, filelist);
+    async submitUpload(param) {
+      // console.log(param.file)
+      const params = new FormData()
+      params.append('username',this.username)
+      // console.log(params.get('username'))
+      params.append('processFile',param.file)
+      // console.log(params.get('processFile'))
+      params.append('processName',this.processName)
+      // console.log(params.get('processName'))
+      // const {data:res} = await this.$http.post("processDefinition/uploadStreamAndDeployment?processName=" + this.processName + "&username=" + this.username, filelist);
+      const {data:res} = await this.$http.post("processDefinition/uploadStreamAndDeployment", params);
       if (res.msg != "success") {
         return this.$message.error("部署流程定义失败！");
       }
       this.editDialogVisible = false;
       return this.$message.success("部署流程定义成功！");
     },
+    beforeUpload(file) {},
     handleRemove(file, fileList) { console.log(file, fileList); },
     handlePreview(file) { console.log(file); },
     showEditDialog() { this.editDialogVisible = true; },
     editDialogClosed() { this.editDialogVisible = false;},
+    async submitImportForm() {
+      this.$refs.upload.submit()
+    },
   }
 }
 </script>
